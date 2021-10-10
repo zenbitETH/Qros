e6c741ece289609f51762e63977a15dd5be7f40b
HabacucMX <habacuc.vera@gmail.com>
2021-10-10 17:33:29 -0500

    Moralis setting

--- frontend/packages/react-app/src/index.js
+++ frontend/packages/react-app/src/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
+
+import { MoralisProvider } from "react-moralis";
 import "./styles/index.css";
 import "./styles/cards.scss";
 import "./styles/profile.scss";
 import App from "./App";
 
-// You should replace this url with your own and put it into a .env file
-// See all subgraphs: https://thegraph.com/explorer/
-const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
-});
+
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  
+  <MoralisProvider appId="TOYUgAQy6p6Sx3uKprDKcRRjU7AXF7DFxdSNlR2y" serverUrl="https://2tmlqxwkknpp.moralishost.com:2053/server">
     <App />
-  </ApolloProvider>,
+  </MoralisProvider>,
+
   document.getElementById("root"),
 );
+
